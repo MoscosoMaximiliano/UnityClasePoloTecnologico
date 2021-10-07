2c3aea43cbeda8c640f270e36c47dff3da3d9afe
MaxiMoscoso <moscosomaximiliano@gmail.com>
2021-10-07 10:09:24 -0300

    Fisicas, Materiales y TAGS, Salto

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -6,7 +6,6 @@ public class MovimientoPersonaje : MonoBehaviour
 {
     public Rigidbody2D _rb;
     public float velocidadPersonaje = 3f;
-    public float fuerzaSalto = 2f;
     
     // Start is called before the first frame update
     void Start()
@@ -18,21 +17,11 @@ public class MovimientoPersonaje : MonoBehaviour
     void Update()
     {
         Movimiento();
-        
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Salto();    
-        }
     }
     
     void Movimiento()
     {
         float movimiento = Input.GetAxis("Horizontal") * velocidadPersonaje;
         _rb.velocity = new Vector2(movimiento, _rb.velocity.y);
-    }
-
-    void Salto() 
-    {
-        _rb.velocity = new Vector2(_rb.velocity.x, fuerzaSalto);
     }
 }
---
+++ Assets/Scripts/Salto.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salto : MonoBehaviour
+{
+    public Rigidbody2D _rb;
+    public float fuerzaSalto = 2f;
+    public bool puedeSaltar = false;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && puedeSaltar == true)
+        {
+            Saltar();    
+        }
+    }
+
+    void Saltar() 
+    {
+        //_rb.AddForce(new Vector2(0, fuerzaSalto));
+        _rb.velocity = new Vector2(_rb.velocity.x, fuerzaSalto);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "Suelo")
+        {
+            puedeSaltar = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "Suelo")
+        {
+            puedeSaltar = false;
+        }
+    }
+}
